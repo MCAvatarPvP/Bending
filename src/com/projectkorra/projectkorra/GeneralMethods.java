a96abedfe0187f18d4c765b2bc932b873e00e47c
zemirek <zemir0email@gmail.com>
2023-04-12 08:15:46 +0200

    armorstands now aren't affected by abilities that are invisible

--- src/com/projectkorra/projectkorra/GeneralMethods.java
+++ src/com/projectkorra/projectkorra/GeneralMethods.java
@@ -769,7 +769,7 @@ public class GeneralMethods {
 	 * @return A list of entities around a point
 	 */
 	public static List<Entity> getEntitiesAroundPoint(final Location location, final double radius) {
-		return getEntitiesAroundPoint(location, radius, entity -> !(entity.isDead() || (entity instanceof Player && ((Player) entity).getGameMode().equals(GameMode.SPECTATOR))) || entity instanceof ArmorStand && ((ArmorStand) entity).isMarker());
+		return getEntitiesAroundPoint(location, radius, entity -> !(entity.isDead() || (entity instanceof Player && ((Player) entity).getGameMode().equals(GameMode.SPECTATOR))) || entity instanceof ArmorStand && ((ArmorStand) entity).isMarker() || entity instanceof ArmorStand && ((ArmorStand) entity).isInvisible() && !ConfigManager.getConfig().getBoolean("Properties.AffectArmorStands"));
 	}
 
 	public static long getGlobalCooldown() {
