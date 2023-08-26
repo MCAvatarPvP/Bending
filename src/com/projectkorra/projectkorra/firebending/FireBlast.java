a3648813bb093d7ac87eda6d0e9f379a28554f33
zemirek <zemir0email@gmail.com>
2023-08-26 10:12:31 +0200

    Revert "Remove FireBlastCharged when using FireBlast"
    This reverts commit a294457871ef2fb0c925f13a0d735b47394d6084.

--- src/com/projectkorra/projectkorra/firebending/FireBlast.java
+++ src/com/projectkorra/projectkorra/firebending/FireBlast.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import com.projectkorra.projectkorra.ability.CoreAbility;
 import com.projectkorra.projectkorra.region.RegionProtection;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -74,9 +73,6 @@ public class FireBlast extends FireAbility {
 		if (location.getBlock().isLiquid()) {
 			return;
 		}
-
-		CoreAbility abil = CoreAbility.getAbility(FireBlastCharged.class);
-		if (abil != null) abil.remove();
 
 		this.setFields();
 		this.safeBlocks = safeBlocks;
